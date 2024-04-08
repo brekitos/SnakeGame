23b79efefb73989f24e7c64612414860926e7894
Nikita Bogatyrev <bogatyrevnikita@bk.ru>
2024-04-08 22:14:43 +0400

    moved program

---
+++ Source/SnakeGame/Intertable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Intertable.h"
+
+// Add default functionality here for any IIntertable functions that are not pure virtual.
